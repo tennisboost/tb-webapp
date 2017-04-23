23723a321de376ac05aa8b42843baf7d114e2bd6
Tom Hill <tomhill98@me.com>
2017-04-23 23:58:54 +1000

    [NOT COMPLETE] Adds basic UserAccount and Comp models

---
+++ models.py
@@ -0,0 +1,16 @@
+from peewee import *
+
+db = SqliteDatabase('test.db')
+
+# Note models should be named the singular, not plural
+class UserAccount(Model):
+    email = CharField(unique=True)
+    password = CharField() # This will be hashed by bcrypt or similar
+
+class Comp(Model):
+    # user = ForeignKeyField(rel_model=UserAccount, related_name='Comp') <-- for when we add in user functionality fully
+    name = CharField()
+    comp_type = CharField()
+    singlesDoubles = CharField()
+    # scores = hmmmm unfortunately only PostgreSQL provides ArrayField or JSONField functionality... hence we might have to switch to it.
+    # I'm too tired to implement that right at this moment...
